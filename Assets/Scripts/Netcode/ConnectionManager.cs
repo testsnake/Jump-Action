1c3ceff4fe6cfb104ac0c6bbe784066fc973683f
Will W <willowhitfield@outlook.com>
2024-10-06 15:19:41 -0700

    Server and Client connections basic setup
    This is pretty complicated but i think i understand so far

--- Assets/Scripts/Netcode/ConnectionManager.cs
+++ Assets/Scripts/Netcode/ConnectionManager.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.NetCode;
 using Unity.Entities;
+using System.Net;
+using Unity.Collections;
+using Unity.Networking.Transport;
 
 public class ConnectionManager : MonoBehaviour
 {
+    //Change this IP address to the address of the dedicated server when we publish!
     [SerializeField] private string ip = "127.0.0.1";
     [SerializeField] private ushort port = 7979;
     public enum Role
@@ -17,6 +21,7 @@ public class ConnectionManager : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        //Check whether we're the server or the client (or both, if we're in the editor)
         if (Application.isEditor)
         {
             role = Role.ServerClient;
@@ -35,6 +40,7 @@ public class ConnectionManager : MonoBehaviour
         World serverWorld = null;
         World clientWorld = null;
 
+        //Set up worlds for server and client
         if(role == Role.ServerClient || role == Role.Server)
         {
             serverWorld = ClientServerBootstrap.CreateServerWorld("ServerWorld");
@@ -44,5 +50,45 @@ public class ConnectionManager : MonoBehaviour
         {
             clientWorld = ClientServerBootstrap.CreateClientWorld("ClientWorld");
         }
+
+        //Delete the default generated world
+        foreach (var world in World.All)
+        {
+            if(world.Flags == WorldFlags.Game)
+            {
+                world.Dispose();
+                break;
+            }
+        }
+
+        if(serverWorld != null)
+        {
+            World.DefaultGameObjectInjectionWorld = serverWorld;
+        } else if (clientWorld != null) 
+        {
+            World.DefaultGameObjectInjectionWorld = clientWorld;
+        }
+
+        if(serverWorld != null)
+        {
+            //Make the server listen for connections
+            using var query = serverWorld.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
+            query.GetSingletonRW<NetworkStreamDriver>().ValueRW.Listen(ClientServerBootstrap.DefaultListenAddress.WithPort(port));
+        }
+        if (clientWorld != null)
+        {
+            //All of this is just to convert from ip and port into an endpoint
+            IPAddress serverAddress = IPAddress.Parse(ip);
+            NativeArray<byte> nativeArrayAddress = new NativeArray<byte>(serverAddress.GetAddressBytes().Length, Allocator.Temp);
+            nativeArrayAddress.CopyFrom(serverAddress.GetAddressBytes());
+            NetworkEndpoint endpoint = NetworkEndpoint.AnyIpv4;
+            endpoint.SetRawAddressBytes(nativeArrayAddress);
+            endpoint.Port = port;
+
+            //Now we can actually send the connection request to the endpoint
+            using var query = clientWorld.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
+            query.GetSingletonRW<NetworkStreamDriver>().ValueRW.Connect(clientWorld.EntityManager, endpoint);
+        }
+
     }
 }
