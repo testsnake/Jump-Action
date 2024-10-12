3748ae90c4023e27bbb0c3254ab67173021be2e7
Will W <willowhitfield@outlook.com>
2024-10-12 13:49:45 -0700

    Switching to NetCode for GameObjects

--- Assets/Scripts/Netcode/InputData.cs
+++ Assets/Scripts/Netcode/InputData.cs
@@ -1,14 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Unity.NetCode;
-using Unity.Mathematics;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Unity.NetCode;
+//using Unity.Mathematics;
 
-[GhostComponent(PrefabType = GhostPrefabType.AllPredicted)]
-public struct PlayerInputData : IInputComponentData
-{
-    public float2 move;
-    public float2 rotation;
-    public InputEvent jump;
-    public InputEvent crouch;
-}
+//[GhostComponent(PrefabType = GhostPrefabType.AllPredicted)]
+//public struct PlayerInputData : IInputComponentData
+//{
+//    public float2 move;
+//    public float2 rotation;
+//    public InputEvent jump;
+//    public InputEvent crouch;
+//}
--- Assets/Scripts/Netcode/Prefabs Manager.cs
+++ Assets/Scripts/Netcode/Prefabs Manager.cs
@@ -1,40 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Entities;
-using Unity.NetCode;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using Unity.Entities;
+//using Unity.NetCode;
+//using UnityEngine;
 
-public class PrefabsManager : MonoBehaviour
-{
-    //Add as many prefabs here as needed. Make sure to also add them to PrefabsData though
-    //Also make sure you assign it in the Bake function
-    public GameObject player = null;
+//public class PrefabsManager : MonoBehaviour
+//{
+//    //Add as many prefabs here as needed. Make sure to also add them to PrefabsData though
+//    //Also make sure you assign it in the Bake function
+//    public GameObject player = null;
     
-    public struct PrefabsData : IComponentData
-    {
-        public Entity player;
-    }
+//    public struct PrefabsData : IComponentData
+//    {
+//        public Entity player;
+//    }
 
-    public class PrefabsBaker : Baker<PrefabsManager>
-    {
-        public override void Bake(PrefabsManager authoring)
-        {
-            //For every new prefab, we add a new Entity here like the playerPrefab
-            Entity playerPrefab = default;
+//    public class PrefabsBaker : Baker<PrefabsManager>
+//    {
+//        public override void Bake(PrefabsManager authoring)
+//        {
+//            //For every new prefab, we add a new Entity here like the playerPrefab
+//            Entity playerPrefab = default;
 
-            //Then do another check just like this one to assign it's value
-            if (authoring.player != null)
-            {
-                playerPrefab = GetEntity(authoring.player, TransformUsageFlags.Dynamic);
-            }
-            var entity = GetEntity(TransformUsageFlags.Dynamic);
+//            //Then do another check just like this one to assign it's value
+//            if (authoring.player != null)
+//            {
+//                playerPrefab = GetEntity(authoring.player, TransformUsageFlags.Dynamic);
+//            }
+//            var entity = GetEntity(TransformUsageFlags.Dynamic);
 
-            //Then finally make sure to add the prefab to this list, like you see here
-            AddComponent(entity, new PrefabsData
-            {
-                player = playerPrefab
-            });
-        }
-    }
+//            //Then finally make sure to add the prefab to this list, like you see here
+//            AddComponent(entity, new PrefabsData
+//            {
+//                player = playerPrefab
+//            });
+//        }
+//    }
 
-}
+//}
